92a7d8de6e84f4930bee44728c2b3b3d8b000395
Kaloyan Pavlov <kaloyan.pavlov@accedia.com>
2019-09-04 15:14:08 +0300

    Tasks are unwrapped in ObjectReflector.GetType

--- test/Tests/Execution/SchemaExecutionTests.cs
+++ test/Tests/Execution/SchemaExecutionTests.cs
@@ -1,6 +1,9 @@
 ﻿using GraphQL;
+using GraphQL.Conventions.Relay;
 using GraphQL.Conventions.Tests;
 using GraphQL.Conventions.Tests.Templates.Extensions;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Tests.Execution
 {
@@ -13,7 +16,8 @@ namespace Tests.Execution
             schema.ShouldHaveQueries(1);
             schema.ShouldHaveMutations(0);
             schema.Query.ShouldHaveFieldWithName("test");
-            var res = schema.Execute((e) => e.Query = "query { test }");
+            var result = schema.Execute((e) => e.Query = "query { test }");
+            ResultHelpers.AssertNoErrorsInResult(result);
         }
 
         class SchemaTypeWithDecimal
@@ -25,5 +29,63 @@ namespace Tests.Execution
         {
             public decimal Test => 10;
         }
+
+        //public class BugReproSchema
+        //{
+        //    public BugReproQuery Query { get; }
+        //}
+
+        //public class BugReproQuery
+        //{
+        //    public BugReproQuery() { }
+
+        //    public Stuff<Broken> A() => new Stuff<Broken> { Value = new Broken() }; // Changes the name of the method to 'Broken' and it will work...
+
+        //    public Stuff<Connection<Holder>> Holders() =>
+        //        new Stuff<Connection<Holder>>
+        //        {
+        //            Value = new Connection<Holder>
+        //            {
+        //                Edges = new List<Edge<Holder>>
+        //                    {
+        //                    new Edge<Holder>
+        //                    {
+        //                        Cursor = Cursor.New<Holder>(0),
+        //                        Node = new Holder
+        //                        {
+        //                        }
+        //                    }
+        //                    },
+        //                PageInfo = new PageInfo
+        //                {
+        //                    EndCursor = Cursor.New<Holder>(0),
+        //                    HasNextPage = false,
+        //                    HasPreviousPage = false,
+        //                    StartCursor = Cursor.New<Holder>(0)
+        //                },
+        //                TotalCount = 1
+        //            }
+        //        };
+        //}
+
+        //public class Holder
+        //{
+        //    public IEnumerable<ICommonInterface> InterfaceConnection() => new[] { new Broken() };
+        //}
+
+        //public interface ICommonInterface
+        //{
+        //    int Test { get; }
+        //}
+
+        //public class Broken : ICommonInterface
+        //{
+        //    public int Test => 1;
+        //}
+
+        //public class Stuff<T>
+        //{
+        //    public T Value { get; set; }
+        //}
     }
 }
